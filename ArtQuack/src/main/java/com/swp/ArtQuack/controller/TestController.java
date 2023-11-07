922096c4c883e3ca4f565ec560f0f2980f740ebf
Aoyamagoshho <duylase171618@fpt.edu.vn>
2023-11-07 14:19:14 +0700

    upload EmailService

---
+++ ArtQuack/src/main/java/com/swp/ArtQuack/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.swp.ArtQuack.controller;
+import com.swp.ArtQuack.service.EmailService;
+import com.swp.ArtQuack.view.EmailDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    EmailService emailService;
+
+    @GetMapping("test")
+    public void sendMail(){
+        EmailDetail emailDetail = new EmailDetail();
+        emailDetail.setRecipient("leanhduy16022002@gmail.com");
+        emailDetail.setSubject("test123");
+        emailDetail.setMsgBody("aaa");
+        emailService.sendMailTemplate(emailDetail);
+    }
+}
--- ArtQuack/src/main/java/com/swp/ArtQuack/service/EmailService.java
+++ ArtQuack/src/main/java/com/swp/ArtQuack/service/EmailService.java
@@ -1,26 +1,43 @@
-//package com.swp.ArtQuack.service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class EmailService {
-//	
-//	private final JavaMailSender mailSender;
-//
-//    @Autowired
-//    public EmailService(JavaMailSender mailSender) {
-//        this.mailSender = mailSender;
-//    }
-//
-//    public void sendSimpleEmail(String to, String subject, String body) {
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(to);
-//        message.setSubject(subject);
-//        message.setText(body);
-//        mailSender.send(message);
-//    }
-//
-//}
+package com.swp.ArtQuack.service;
+
+import com.swp.ArtQuack.view.EmailDetail;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    public void sendMailTemplate(EmailDetail emailDetail){
+        try{
+            Context context = new Context();
+
+            context.setVariable("name", "Anh Duy");
+
+            String text = templateEngine.process("emailtemplate", context);
+
+            // Creating a simple mail message
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+
+            // Setting up necessary details
+            mimeMessageHelper.setFrom("admin@gmail.com");
+            mimeMessageHelper.setTo(emailDetail.getRecipient());
+            mimeMessageHelper.setText(text, true);
+            mimeMessageHelper.setSubject(emailDetail.getSubject());
+            javaMailSender.send(mimeMessage);
+        }catch (MessagingException messagingException){
+            messagingException.printStackTrace();
+        }
+    }
+}
